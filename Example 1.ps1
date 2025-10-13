087a37c27505fa9708740b2dd86e74724df04b4a
Marius Solbakken Mellum <marius.solbakken@fortytwo.io>
2025-10-13 12:44:32 +0200

    asd
    Signed-off-by: Marius Solbakken Mellum <marius.solbakken@fortytwo.io>

--- Example 1.ps1
+++ Example 1.ps1
@@ -4,7 +4,7 @@ Import-Module .\Fortytwo.IAM.UserWriteback\ -Force
 $cs ??= Read-Host -AsSecureString
 Add-EntraIDClientSecretAccessTokenProfile `
     -TenantId "237098ae-0798-4cf9-a3a5-208374d2dcfd" `
-    -ClientId "72004b2d-a082-4d29-b51d-85011dbdccb4" `
+    -ClientId "55ffa0ca-c74f-4344-bf0e-af56ff30f920" `
     -ClientSecret $cs
     
 Connect-ChangeEmailAgent
