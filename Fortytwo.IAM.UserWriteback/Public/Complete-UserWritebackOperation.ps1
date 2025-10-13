8babde834521a7399a37d7f39f06c216dd3822d3
Marius Solbakken Mellum <marius.solbakken@fortytwo.io>
2025-10-13 15:10:25 +0200

    test
    Signed-off-by: Marius Solbakken Mellum <marius.solbakken@fortytwo.io>

--- Fortytwo.IAM.UserWriteback/Public/Complete-UserWritebackOperation.ps1
+++ Fortytwo.IAM.UserWriteback/Public/Complete-UserWritebackOperation.ps1
@@ -7,17 +7,33 @@ function Complete-UserWritebackOperation {
     )
 
     Process {
-        if($Operation.Action -eq "New-ADUser") {
+        if ($Operation.Action -eq "New-ADUser") {
             $Operation | Show-UserWritebackOperation -Single
             $Password = ConvertTo-SecureString -String (New-Guid).ToString() -AsPlainText -Force
             $Parameters = $Operation.Parameters
             $CreatedUser = New-ADUser -AccountPassword $Password @Parameters -Passthru
 
-            if($CreatedUser.distinguishedName) {
+            if ($CreatedUser.distinguishedName) {
                 Write-Verbose "Created new AD user '$($CreatedUser.SamAccountName)' with distinguished name '$($CreatedUser.DistinguishedName)'."
-            } else {
+
+                # TODO: UPDATE ENTRA ID USER HERE?
+            }
+            else {
                 Write-Warning "Failed to create new AD user with parameters: $($Parameters | Out-String)"
             }
+        }
+        elseif ($Operation.Action -eq "Set-ADUser") {
+            $Operation | Show-UserWritebackOperation -Single
+            $Parameters = $Operation.Parameters
+            Set-ADUser -Identity $Operation.Identity @Parameters
+
+            Write-Verbose "Updated AD user '$($Operation.Identity)'."
+        }
+        elseif ($Operation.Action -eq "Remove-ADUser") {
+            $Operation | Show-UserWritebackOperation -Single
+            Remove-ADUser -Identity $Operation.Identity -Confirm:$false
+
+            Write-Verbose "Removed AD user '$($Operation.Identity)'."
         }
     }
 }
